64bd6ee3506a4da42ce3819111448744beeeeeb9
Alexander Shabunevich <loonar@gmail.com>
2023-10-25 16:32:05 +0300

    Build settings updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import svgLoader from 'vite-svg-loader'
+import banner from 'vite-plugin-banner'
+
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), svgLoader()]
+  build: {
+    lib: {
+      name: 'trilist',
+      formats: ['umd'],
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: (format) => `${pkg.name}.${format}.js`
+    }
+  },
+  plugins: [
+    svelte(),
+    svgLoader(),
+    banner(
+      `/*! ${pkg.name} v${pkg.version} | (c) ${pkg.author} | Released under the ${pkg.license} license */`
+    )
+  ]
 })

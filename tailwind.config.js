2d8214b8ae47332cb85617221edbe8e6e4923aa7
Alexander Shabunevich <loonar@gmail.com>
2023-11-02 11:21:15 +0300

    Refactor css variables code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,33 +6,24 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
+    colors: {
+      'trilist-primary': `var(--trilist-colors-primary, ${colors.blue[600]})`,
+      'trilist-primary-text': `var(--trilist-colors-primary-text, ${colors.white})`,
+      'trilist-secondary': `var(--trilist-colors-secondary, ${colors.gray[200]})`,
+      'trilist-input-border': `var(--trilist-colors-input-border, ${colors.gray[400]})`,
+      'trilist-input-focus': `var(--trilist-colors-input-focus, ${colors.blue[600]})`,
+      'trilist-input-fone': `var(--trilist-colors-input-fone, ${colors.white})`,
+      'trilist-modal-fone': `var(--trilist-colors-modal-fone, ${colors.white})`,
+      'trilist-icon': `var(--trilist-colors-icon, ${colors.gray[400]})`,
+      'trilist-icon-hover': `var(--trilist-colors-icon-hover, ${colors.gray[500]})`
+    },
     extend: {
-      colors: {
-        'trilist-primary':
-          'var(--trilist-color-primary, ' + colors.blue[600] + ')',
-        'trilist-primary-text':
-          'var(--trilist-color-primary-text, ' + colors.white + ')',
-        'trilist-secondary':
-          'var(--trilist-color-secondary, ' + colors.gray[200] + ')',
-        'trilist-input-border':
-          'var(--trilist-color-input-border, ' + colors.gray[400] + ')',
-        'trilist-input-focus':
-          'var(--trilist-color-input-focus, ' + colors.blue[600] + ')',
-        'trilist-input-fone':
-          'var(--trilist-color-input-fone, ' + colors.white + ')',
-        'trilist-modal-fone':
-          'var(--trilist-color-modal-fone, ' + colors.white + ')',
-        'trilist-icon': 'var(--trilist-color-icon, ' + colors.gray[400] + ')',
-        'trilist-icon-hover':
-          'var(--trilist-color-icon-hover, ' + colors.gray[500] + ')'
-      },
       borderRadius: {
-        DEFAULT:
-          'var(--trilist-border-radius, ' + defaultTheme.borderRadius.md + ')'
+        DEFAULT: `var(--trilist-border-radius, ${defaultTheme.borderRadius.md})`
       },
       fontSize: {
-        base: 'var(--trilist-text-base, 16px)',
-        sm: 'var(--trilist-text-sm, 14px)'
+        base: `var(--trilist-text-base, 16px)`,
+        sm: `var(--trilist-text-sm, 14px)`
       }
     }
   },

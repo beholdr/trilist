da0856847b024527723e3ca8e2eeffa58267e27d
Alexander Shabunevich <loonar@gmail.com>
2023-11-08 17:36:48 +0300

    Change build format to esm

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+import { transform } from 'esbuild'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import svgLoader from 'vite-svg-loader'
 import banner from 'vite-plugin-banner'
@@ -7,19 +8,46 @@ import dts from 'vite-plugin-dts'
 
 import pkg from './package.json'
 
+// https://github.com/vitejs/vite/issues/6555#issuecomment-1342664357
+const minifyEs = () => {
+  return {
+    name: 'minifyEs',
+    renderChunk: {
+      order: 'post',
+      async handler(code, chunk, outputOptions) {
+        if (
+          outputOptions.format === 'es' &&
+          chunk.fileName.endsWith('.min.js')
+        ) {
+          return await transform(code, { minify: true })
+        }
+        return code
+      }
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      name: 'trilist',
-      formats: ['umd'],
+      formats: [
+        'es',
+        // @ts-expect-error
+        'esm'
+      ],
       entry: resolve(__dirname, 'src/main.ts'),
-      fileName: (format) => `${pkg.name}.${format}.js`
+      fileName: (format) =>
+        ({
+          es: `${pkg.name}.js`,
+          esm: `${pkg.name}.min.js`
+        }[format])
     }
   },
   plugins: [
     svelte(),
     svgLoader(),
+    minifyEs(),
     dts({
       outDir: 'dist/types',
       include: 'src/lib',
